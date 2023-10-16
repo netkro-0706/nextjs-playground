0cbccbd80d9fe1110cf1e948d536de9f3271e0ab
netkro <kroging@gmail.com>
2023-10-16 18:59:50 +0900

    just git add

---
+++ pages/contact-us.tsx
@@ -0,0 +1,12 @@
+function ContactPage() {
+  return (
+    <div>
+      <ul>
+        <li>Email: mymail@example.com</li>
+        <li>name: name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactPage;
